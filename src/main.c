05c620acbe6d126a9382e89411de4eaba3244a56
lhalvarez96 <lhalvarezrivera@gmail.com>
2017-08-14 22:49:23 -0300

    programa pronto

--- src/main.c
+++ src/main.c
@@ -6,17 +6,51 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+
 
 int main() {
 
-  char c;
+  char c[300];
+  int i, j=0, k=0; 
+  int tamanho(char c[]);
+  
+  fgets(c,300,stdin);
 
-  c = 1;
+  k= tamanho(c);
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+  for(i=0; i<=k; i++){
+	
+   if((c[i+1]<'0')||((c[i+1]>'9') && (c[i+1]<'A')) || ((c[i+1]>'Z') && (c[i+1]<'a')) || (c[i+1]>'z')){
+        if((c[i]>='0')&&(c[i]<='9')){
+		j++;
+		}
+	if((c[i]>='A')&&(c[i]<='Z')){
+                j++;
+                }
+        if((c[i]>='a')&&(c[i]<='z')){
+                j++;
+                }
+        }
+        
+     
 }
+
+	printf("%d\n", j);
+
+return 0;
+  
+
+  
+}
+
+int tamanho (char  c[]){
+  int tam=0;
+ 
+     while (c[tam] != '\0'){
+           tam++;
+          }
+      return tam;
+}
+
+
